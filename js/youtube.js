76a09d79ac641da71ee0f3b4b1479487bcc4cb05
RRickkert <RRickkert@hotmail.com>
2018-08-27 23:26:45 +0200

    Added youtube videos

---
+++ js/youtube.js
@@ -0,0 +1,83 @@
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+function onYouTubeIframeAPIReady() {
+	var ytVideoElements = document.getElementsByClassName("youtube-video");
+	var ytPlayers = [];
+
+	for (var i = 0; i < ytVideoElements.length; i++) {
+		ytPlayers.push({
+			id: ytVideoElements[i].id,
+			videoId: ytVideoElements[i].id
+		})
+	}
+
+	ytPlayers.forEach(function(playerInfo) {
+		createPlayer(playerInfo);
+	});
+}
+
+function createPlayer(playerInfo) {
+	var parentElement = document.getElementById(playerInfo.id).parentElement;
+	for (var i = 0; i < parentElement.childNodes.length; i++) {
+		var el = parentElement.childNodes[i];
+		var classes = el.className != undefined ? el.className.split(" ") : [];
+		for (var j = 0, jl = classes.length; j < jl; j++) {
+			if (classes[j] == "overlay") {
+				var player = new YT.Player(playerInfo.id, {
+					width: '960',
+					height: '540',
+					videoId: playerInfo.videoId, //  B7bqAsxee4I
+					host: 'https://www.youtube.com',
+					playerVars: {
+						'autoplay': 1,
+						'controls': 0,
+						'start': 0
+					},
+					events: {
+						'onReady': onPlayerReady,
+						'onStateChange': function(event) {
+							onPlayerStateChange(el, event);
+						}
+					}
+				});
+				return;
+			}
+		}
+	}
+}
+
+function onPlayerReady(event) {
+	event.target.setVolume(0);
+	event.target.playVideo();
+}
+
+function onPlayerStateChange(overlay, event) {
+	if (event.data == YT.PlayerState.PLAYING) {
+		window.setTimeout(function() {
+			hideOverlay(overlay, event);
+		}, 500);
+
+		window.setTimeout(function() {
+			showOverlay(overlay, event);
+		}, event.target.getDuration() * 1000 - 1000 - 100);
+	}
+
+	if (event.data == YT.PlayerState.ENDED) {
+		window.setTimeout(function() {
+			event.target.playVideo();
+		}, 2500);
+	}
+}
+
+function showOverlay(overlay, event) {
+	overlay.classList.add("show");
+	overlay.classList.remove("hide");
+}
+
+function hideOverlay(overlay, event) {
+	overlay.classList.add("hide");
+	overlay.classList.remove("show");
+}
