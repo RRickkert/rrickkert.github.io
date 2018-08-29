56b0639621e2173cf38138fa69cceb937e15eb85
RRickkert <RRickkert@hotmail.com>
2018-08-29 16:31:25 +0200

    Cleaned up a bit, added mp4 for unsupported browsers

--- js/scroll.js
+++ js/scroll.js
@@ -1,6 +1,6 @@
 function scrollTo(element, duration) {
 
 	$([document.documentElement, document.body]).animate({
-		scrollTop: $(element).offset().top - document.getElementsByClassName("navbar")[0].clientHeight
+		scrollTop: $(element).offset().top - document.getElementById("navbar").clientHeight
 	}, duration);
 }
