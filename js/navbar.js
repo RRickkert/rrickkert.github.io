f90c365264dcc59d3af322f93a92ab6c7363aaaf
RRickkert <RRickkert@hotmail.com>
2018-09-01 00:27:32 +0200

    Couple of small fixes

--- js/navbar.js
+++ js/navbar.js
@@ -6,18 +6,26 @@ var isScrolling = false;
 // Get all buttons with class="btn" inside the container
 var btns = btnContainer.getElementsByClassName("btn");
 // Loop through the buttons and add the active class to the current/clicked button
+var timeoutHandle;
+
 for (var i = 0; i < btns.length; i++) {
 	btns[i].addEventListener("click", function() {
 		if (currentActiveMenuButton == this)
 			return;
 		currentActiveMenuButton.className = currentActiveMenuButton.className.replace(" active", "");
+		currentMenuButtonInView.className = currentMenuButtonInView.className.replace(" in-view", "");
 		currentActiveMenuButton = this;
+		currentMenuButtonInView = this;
 		currentActiveMenuButton.className += " active";
 
+		$([document.documentElement, document.body]).stop();
 		scrollTo(document.getElementById(currentActiveMenuButton.getAttribute("target")), 1000, 65);
 		isScrolling = true;
 
-		setTimeout(function() {
+		if (timeoutHandle != undefined)
+			clearTimeout(timeoutHandle);
+
+		timeoutHandle = setTimeout(function() {
 			isScrolling = false;
 		}, 1000);
 	});
@@ -28,12 +36,12 @@ var animating = false;
 function magic() {
 	for (var i = 0; i < btns.length; i++) {
 		if ($(window).scrollTop() >= $(document.getElementById(btns[i].getAttribute("target"))).offset().top - 65 - 1 - document.getElementById("navbar").clientHeight) {
-
-			currentMenuButtonInView.className = currentMenuButtonInView.className.replace(" in-view", "");
-			currentMenuButtonInView = btns[i];
-			currentMenuButtonInView.className += " in-view";
-
 			if (!isScrolling) {
+				currentMenuButtonInView.className = currentMenuButtonInView.className.replace(" in-view", "");
+				currentMenuButtonInView = btns[i];
+				currentMenuButtonInView.className += " in-view";
+
+
 				currentActiveMenuButton.className = currentActiveMenuButton.className.replace(" active", "");
 				currentActiveMenuButton = btns[i];
 				currentActiveMenuButton.className += " active";
